c29e195d7cafa53f6f4a8072d66a72e7c97b7205
Marcelo Bartsch <ruhlem@amazon.com>
2018-04-18 13:49:39 +0100

    initial commit.

---
+++ eks-cluster.sh
@@ -0,0 +1,60 @@
+#!/bin/bash 
+
+if [ -z ${KUBECTL_PLUGINS_LOCAL_FLAG_CLUSTER_NAME} ] && [ "${KUBECTL_PLUGINS_LOCAL_FLAG_ACTION}" != "list" ]; then
+  echo "Cluster name must be specified"
+  exit 1
+fi
+
+cluster=${KUBECTL_PLUGINS_LOCAL_FLAG_CLUSTER_NAME}
+region=${KUBECTL_PLUGINS_LOCAL_FLAG_REGION}
+
+list_cluster() {
+  aws --region us-west-2 eks list-clusters --output text
+}
+
+describe_cluster(){
+  aws --region ${region} eks describe-cluster --cluster-name ${cluster} --output text
+}
+delete_cluster (){
+  echo aws --region ${region} eks delete-cluster --cluster-name ${cluster} 
+}
+
+create_cluster() {
+  SUBNETS=$(aws ec2 describe-subnets --region ${region} --filter Name=tag-key,Values="kubernetes.io/cluster/${cluster}" --query 'Subnets[].SubnetId' --output text)
+  SECGROUPS=$(aws ec2 describe-security-groups --region ${region} --filter Name=tag-key,Values="kubernetes.io/cluster/${cluster}" --query 'SecurityGroups[].GroupId' --output text)
+  ARN=$(aws iam list-roles --query 'Roles[?AssumeRolePolicyDocument.Statement[?Principal.Service==`eks.amazonaws.com`]]| [0]| Arn')
+  echo ${SUBNETS}
+  echo ${SECGROUPS}
+  echo "This will create the cluster ${cluster} on the region ${region} using the follwing"
+  echo "Subnets:"
+  echo -e "\t${SUBNETS}"
+  echo "Security Groups"
+  echo -e "\t${SECGROUPS}"
+  echo "Service Role Arn"
+  echo -e "\t${ARN}"
+  exit 0
+}
+
+case ${KUBECTL_PLUGINS_LOCAL_FLAG_ACTION} in
+  list)
+    list_cluster
+    ;;
+  describe)
+    describe_cluster
+    ;;
+  create)
+    create_cluster
+    ;;
+  delete)
+    delete_cluster
+    ;;
+  *)
+    echo "Bad Parameters"
+    exit 2
+esac
+exit
+
+
+
+
+exit 0
