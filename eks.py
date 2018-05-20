d11f946bc1ce552a900de542cf08c086534794db
Marcelo Bartsch <marcelo@bartsch.cl>
2018-05-20 05:39:53 +0100

    change logger format

--- eks.py
+++ eks.py
@@ -8,19 +8,24 @@ THIS IS A POC  IS NOT AT ALL FINISHED!!!!!!!!!
 
 """
 
-import os
-import logging
 import argparse
+import logging
+import os
 import subprocess
-import yaml
+
 import boto3
 import jmespath
+import yaml
+
 #from botocore.exceptions import ClientError, ParamValidationError#, ResourceNotFoundException
 #import botocore.errorfactory
 global REGION
 FORMAT = "%(levelname)s (Line: %(lineno)04d): %(message)s"
+FORMAT = "%(asctime)s - %(name)s - (Line: %(lineno)04d) - %(levelname)s - %(message)s"
 logging.basicConfig(format=FORMAT, level=logging.DEBUG)
+
 #logging.basicConfig(level=logging.DEBUG)
+logging.getLogger(__name__)
 logging.getLogger('boto3').setLevel(logging.INFO)
 logging.getLogger('botocore').setLevel(logging.CRITICAL)
 logging.getLogger('requests').setLevel(logging.INFO)
@@ -362,7 +367,7 @@ def main():
   parser_list.set_defaults(func=list_cluster)
 
   parser_help = subparsers.add_parser('help', help="Show Help")
-
+  parser_help.set_defaults()
   ## Create a EKS Cluster
   parser_create = subparsers.add_parser('create', help="Create EKS Cluster")
   parser_create.add_argument('--cluster-name',
