af0efb2ad52f070cf324b6e6970c60a44281a88b
Marcelo Bartsch <marcelo@bartsch.cl>
2018-08-05 13:29:07 +0100

    add support for --node flag on get nodes, to get a single node instead of all nodes in the cluster

--- eks-getnodes.sh
+++ eks-getnodes.sh
@@ -2,13 +2,18 @@
 
 cluster=${KUBECTL_PLUGINS_LOCAL_FLAG_CLUSTER_NAME}
 region=${KUBECTL_PLUGINS_LOCAL_FLAG_REGION}
+node=${KUBECTL_PLUGINS_LOCAL_FLAG_NODE}
 
 list_nodes() {
   NODES=$(kubectl get nodes -o jsonpath='{.items..metadata.name}')
-  for node in ${NODES} ; do
-    echo -n "${node} / "
+  if [ ! -z ${node} ] ;then 
     aws --region ${region} ec2 describe-instances --output json --query "Reservations[?Instances[?PrivateDnsName==\`${node}\`]]" | jq -r '.[].Instances[0].PublicDnsName'
-  done
+  else 
+    for node in ${NODES} ; do
+      echo -n "${node} / "
+      aws --region ${region} ec2 describe-instances --output json --query "Reservations[?Instances[?PrivateDnsName==\`${node}\`]]" | jq -r '.[].Instances[0].PublicDnsName'
+    done
+  fi
 }
 
-list_nodes
+list_nodes $node
